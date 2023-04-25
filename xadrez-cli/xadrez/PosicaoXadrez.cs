bde176767b37d3fe49aa57837f55f73bb3fb44e7
Vinicius Jara <viniciusfjara@gmail.com>
2023-04-25 13:10:42 -0400

    criando a classe PosicaoXadrez

---
+++ xadrez-cli/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using xadrez_cli.tabuleiro;
+
+namespace xadrez_cli.xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
